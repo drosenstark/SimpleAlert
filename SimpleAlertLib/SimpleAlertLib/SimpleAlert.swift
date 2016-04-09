137baf86a6f6a8bbfe29a264cbd6b555c8a71265
Dan Rosenstark <dr.git@confusionists.com>
2016-04-09 19:36:58 -0400

    - Bug fix for button handler - Fixes for new variations via subclasses

--- SimpleAlertLib/SimpleAlertLib/SimpleAlert.swift
+++ SimpleAlertLib/SimpleAlertLib/SimpleAlert.swift
@@ -16,7 +16,7 @@ import Cartography
 @objc(SimpleAlert)
 public class SimpleAlert: UIView {
 
-    
+
     let messageLabel = UILabel()
     let titleLabel = UILabel()
     let box = UIView()
@@ -28,16 +28,21 @@ public class SimpleAlert: UIView {
     public var titleTextColor  : UIColor!
     public var messageTextColor  : UIColor!
     public var buttonHighlightColor  : UIColor!
+    public var otherViewBoxColor : UIColor!
 
-    let margin = CGFloat(20.0)
+    public var topMargin = CGFloat(20.0)
+    public var bottomMarginIfNecessary = CGFloat(20.0)
+    public var sideMargin = CGFloat(20.0)
+    public var spaceBetweenSections = CGFloat(10.0)
+    public var otherViewInset = CGFloat(0.0)
     let titleHeight = CGFloat(30.0)
-    let otherViewRowHeight = CGFloat(40.0)
-    let otherViewRowSpace = CGFloat(0.5)
-    
+    public var otherViewRowHeight = CGFloat(40.0)
+    public var otherViewRowSpace = CGFloat(0.5)
+
     // working around a shared dependency on other stuff in my own libs
     public var doThisToEveryButton: ((UIButton)->())?
 
-    
+
     // MARK: - class methods
     public class func makeAlert(title: String, message: String) -> SimpleAlert {
         let retVal = SimpleAlert()
@@ -46,64 +51,61 @@ public class SimpleAlert: UIView {
         retVal.theme = .Dark
         return retVal
     }
-    
-    
-    
+
     public var title : String? {
         didSet {
             titleLabel.text = title
         }
     }
-    
+
     public var message : String? {
         didSet {
             messageLabel.text = message
         }
     }
-    
+
     // MARK: - Add Methods
     public func addButtonWithTitle(title: String, block: ()->()) -> UIButton {
         return setupButtonWithText(title, handler: block)
     }
-    
+
 //    public func addCancelButtonWithTitle(title: String, block: ()->()) -> UIButton {
 //        return setupButtonWithText(title, handler: block)
 //    }
-//    
+//
 //    public func addDestructiveButtonWithTitle(title: String, block: ()->()) -> UIButton {
 //        return setupButtonWithText(title, handler: block)
 //    }
-//    
+//
     public func showInWindow(window: UIWindow)  {
         window.addSubview(self)
         self.frame = window.bounds
         self.prepSubviews()
     }
-    
+
     // the initial value is for Objective-C to work
     public var theme : SimpleAlertTheme = .Dark {
         didSet {
             if (theme == .Dark) {
                 boxBackgroundColor = UIColor.blackColor()
                 titleTextColor = UIColor.whiteColor()
-                messageTextColor = UIColor.whiteColor()
                 buttonHighlightColor = UIColor.lightGrayColor()
             } else {
                 boxBackgroundColor = UIColor.whiteColor()
                 titleTextColor = UIColor.blackColor()
-                messageTextColor = UIColor.blackColor()
                 buttonHighlightColor = UIColor.lightGrayColor()
-            
             }
+            messageTextColor = titleTextColor
+            otherViewBoxColor = buttonHighlightColor
         }
-    
+
     }
-    
-    
-    
-    
+
+
+
+
     func setupButtonWithText(text: String, handler: ()->()) -> UIButton {
-        let button = ButtonSub(handler: handler)
+        let button = ButtonSub.makeButtonSub(handler)
         if let doThis = self.doThisToEveryButton {
             doThis(button)
         }
@@ -115,14 +117,16 @@ public class SimpleAlert: UIView {
         button.addTarget(self, action: #selector(SimpleAlert.handleButtonTouchUpInside(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         otherViewsBox.addSubview(button)
         otherViews.append(button)
-        
-        
+
+
         return button
     }
-    
-    func prepSubviews() {
+
+
+    // public for being overridden
+    public func prepSubviews() {
         box.layer.cornerRadius = 10.0
-        
+
         backgroundColor = modalBackgroundColor
         box.backgroundColor = boxBackgroundColor
         box.clipsToBounds = true
@@ -141,22 +145,21 @@ public class SimpleAlert: UIView {
         box.addSubview(messageLabel)
 
         box.addSubview(otherViewsBox)
-        otherViewsBox.backgroundColor = buttonHighlightColor
+        otherViewsBox.backgroundColor = otherViewBoxColor
         otherViewsBox.clipsToBounds = true
-        
-        let spaceBetweenSections = CGFloat(0.5) * margin
+
         let otherViewRowTotalHeight = otherViewRowHeight + otherViewRowSpace
-        
+
 
         let otherViewsBoxHeight = otherViewRowTotalHeight * CGFloat(otherViews.count)
-        
+
         constrain(self, box, titleLabel, messageLabel, otherViewsBox) { view, box, titleLabel, messageLabel, otherViewsBox in
 
             view.size == view.superview!.size
             box.width == 250
             box.center == view.center
-            
-            titleLabel.width == box.width - margin * 2
+
+            titleLabel.width == box.width - sideMargin * 2
             titleLabel.centerX == box.centerX
             titleLabel.height == titleHeight
 
@@ -165,40 +168,39 @@ public class SimpleAlert: UIView {
 
             otherViewsBox.width == box.width
             otherViewsBox.centerX == box.centerX
-            
-            
-            titleLabel.top == box.top + margin
+
+
+            titleLabel.top == box.top + topMargin
             messageLabel.top == titleLabel.bottom + spaceBetweenSections
 
             otherViewsBox.height == otherViewsBoxHeight
             otherViewsBox.top == messageLabel.bottom + spaceBetweenSections
-            
-            var boxHeightWithoutMessage = margin + titleHeight  + 2 * spaceBetweenSections
-            boxHeightWithoutMessage += (otherViewsBoxHeight > 0) ? otherViewsBoxHeight : margin 
-            
+
+            var boxHeightWithoutMessage = topMargin + titleHeight  + 2 * spaceBetweenSections
+            boxHeightWithoutMessage += (otherViewsBoxHeight > 0) ? otherViewsBoxHeight + otherViewInset * 0.25 : bottomMarginIfNecessary
+
             box.height == messageLabel.height + boxHeightWithoutMessage
-            
+
         }
-        
+
         for (index, otherView) in otherViews.enumerate() {
             // change button color
             if let button = otherView as? UIButton {
                 handleButtonTouchUp(button)
             }
-            
-            
-            let top = otherViewRowSpace + CGFloat(CGFloat(index) * otherViewRowTotalHeight)
+
+            let top = otherViewRowSpace + CGFloat(CGFloat(index) * otherViewRowTotalHeight + 0.25 * otherViewInset)
             constrain(otherViewsBox, otherView) { otherViewsBox, otherView in
-                otherView.height == otherViewRowHeight
-                otherView.width == otherViewsBox.width
+                otherView.height == otherViewRowHeight - 0.5 * otherViewInset
+                otherView.width == otherViewsBox.width - otherViewInset
                 otherView.centerX == otherViewsBox.centerX
                 otherView.top == otherViewsBox.top + top
             }
-        
+
         }
-        
+
     }
-    
+
     public func dismiss() {
         self.removeFromSuperview()
     }
@@ -207,51 +209,45 @@ public class SimpleAlert: UIView {
     func handleButtonTouch(button: UIButton) {
         button.backgroundColor = buttonHighlightColor
     }
-    
+
     func handleButtonTouchUp(button: UIButton) {
         button.backgroundColor = boxBackgroundColor
     }
     func handleButtonTouchUpInside(button: UIButton) {
         self.dismiss()
     }
-    
-    
-    
+
+
+
 }
 
 class ButtonSub : UIButton {
-    
+
     var handler: (()->())?
-    
+
+
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
-    convenience init(handler: ()->()) {
-        self.init(frame: CGRectZero)
-        self.handler = handler
-        self.addTarget(self, action: #selector(callHandler), forControlEvents: .TouchUpInside)
+
+    class func makeButtonSub(handler: ()->()) -> ButtonSub {
+        let retVal = ButtonSub(type: .Custom)
+        retVal.handler = handler
+        retVal.addTarget(retVal, action: #selector(callHandler), forControlEvents: .TouchUpInside)
+        return retVal
     }
-    
+
     init() {
         super.init(frame: CGRectZero)
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
     func callHandler() {
         handler?()
     }
-    
-    override func didMoveToSuperview() {
-        
-    }
-    
-    deinit { print("Yeah I deinited as a button") }
-    
+
 
 }
-
-
