cba75ae47bd835cac5a617ecef8bde2a5e2618d7
Dan Rosenstark <dr.git@confusionists.com>
2016-04-11 09:10:50 -0400

    - Fixed tester

--- SimpleAlertExample/SimpleAlertExample/ViewController.swift
+++ SimpleAlertExample/SimpleAlertExample/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 import SimpleAlertLib
 
 class ViewController: UIViewController {
-
+    
     override func viewDidAppear(animated: Bool) {
-        let alert = SimpleAlert(title: "Very Simple", message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam quam, posuere eu diam ut, imperdiet bibendum magna. Integer ut luctus enim, vel fermentum enim. Aenean elementum cursus metus, sit amet\n\niaculis tellus suscipit ac. Cras nec ex in ex auctor convallis. Nullam fermentum quam nibh, eget iaculis sapien eleifend eu. Proin arcu diam, laoreet non egestas nec, bibendum non neque.\n\nAre you really sure you want to do this?")
-        alert.addButtonWithTitle("I'm insane!") { 
+        let alert = SimpleAlert.makeAlert("Very Simple", message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam quam, posuere eu diam ut, imperdiet bibendum magna. Integer ut luctus enim, vel fermentum enim. Aenean elementum cursus metus, sit amet\n\niaculis tellus suscipit ac. Cras nec ex in ex auctor convallis. Nullam fermentum quam nibh, eget iaculis sapien eleifend eu. Proin arcu diam, laoreet non egestas nec, bibendum non neque.\n\nAre you really sure you want to do this?")
+        alert.addButtonWithTitle("I'm insane!") {
             print("yeah I'm out of my head")
         }
         alert.addButtonWithTitle("Show me another Alert") {
-            let alert2 = SimpleAlert(title:"Another Alert", message: "Different theme, get it?");
-            alert2.addButtonWithTitle("OK", block: { 
+            let alert2 = SimpleAlert.makeAlert("Another Alert", message: "Different theme, get it?");
+            alert2.addButtonWithTitle("OK", block: {
                 print("Okay")
             })
             alert2.theme = .Light
@@ -33,8 +33,8 @@ class ViewController: UIViewController {
         alert.showInWindow(self.view.window!)
         
     }
-
-
-
+    
+    
+    
 }
 
