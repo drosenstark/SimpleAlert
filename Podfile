21c024890c14cb73cb574163c9688dd8696bfefb
Dan Rosenstark <dan@dr2050.com>
2020-12-27 16:27:14 -0500

    To cocoapods from Carthage

---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.0'
+
+workspace 'SimpleAlertExample'
+
+target 'SimpleAlertLib' do
+    use_frameworks!
+    pod 'Cartography'
+end
+
+target 'SimpleAlertExample' do
+    project 'SimpleAlertExample/SimpleAlertExample'
+    use_frameworks!
+    pod 'Cartography'
+end
--- SimpleAlertLib/SimpleAlertLib.h
+++ SimpleAlertLib/SimpleAlertLib.h
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for SimpleAlertLib.
+// ! Project version number for SimpleAlertLib.
 FOUNDATION_EXPORT double SimpleAlertLibVersionNumber;
 
-//! Project version string for SimpleAlertLib.
+// ! Project version string for SimpleAlertLib.
 FOUNDATION_EXPORT const unsigned char SimpleAlertLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SimpleAlertLib/PublicHeader.h>
