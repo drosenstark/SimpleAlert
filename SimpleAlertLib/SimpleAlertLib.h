a5dd810be9065c9332c807704c89530039cfcb46
Dan Rosenstark <dan@dr2050.com>
2021-01-03 12:51:05 -0500

    uncrustify

--- SimpleAlertLib/SimpleAlertLib.h
+++ SimpleAlertLib/SimpleAlertLib.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SimpleAlertLibVersionNumber;
 FOUNDATION_EXPORT const unsigned char SimpleAlertLibVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SimpleAlertLib/PublicHeader.h>
-
-
