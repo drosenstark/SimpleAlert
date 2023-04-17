6dae6565220dbdbd5df5243810cbffa7ddb62047
Dan Rosenstark <dan@dr2050.com>
2023-04-17 18:32:10 -0400

    use safe layout guides

--- SimpleAlertLib/UIView+Util.swift
+++ SimpleAlertLib/UIView+Util.swift
@@ -6,14 +6,14 @@ public extension UIView {
     func constrainCenterTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
         guard let view else { return }
 
-        centerXAnchor.constraint(equalToSystemSpacingAfter: view.centerXAnchor, multiplier: multiplierX).isActive = true
-        centerYAnchor.constraint(equalToSystemSpacingBelow: view.centerYAnchor, multiplier: multiplierY).isActive = true
+        centerXAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.centerXAnchor, multiplier: multiplierX).isActive = true
+        centerYAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.centerYAnchor, multiplier: multiplierY).isActive = true
     }
 
     func constrainSizeTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
         guard let view else { return }
 
-        widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: multiplierX).isActive = true
-        heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: multiplierY).isActive = true
+        widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: multiplierX).isActive = true
+        heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: multiplierY).isActive = true
     }
 }
