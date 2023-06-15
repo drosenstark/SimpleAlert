0dae822ed69dce5fc0dc92b9c0dd9fb022c7a22d
Dan Rosenstark <dan@dr2050.com>
2023-06-15 11:28:36 -0400

    adjust visibility

--- SimpleAlertLib/UIView+Util.swift
+++ SimpleAlertLib/UIView+Util.swift
@@ -3,14 +3,14 @@
 import UIKit
 
 extension UIView {
-    func constrainCenterTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
+    public func constrainCenterTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
         guard let view else { return }
 
         NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerX, multiplier: multiplierX, constant: 0).activateAndName("simpleAlert.box.centerX")
         NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerY, multiplier: multiplierY, constant: 0).activateAndName("simpleAlert.box.centerY")
     }
 
-    func constrainSizeTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
+    public func constrainSizeTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
         guard let view else { return }
 
         NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .width, multiplier: multiplierX, constant: 0).activateAndName("simpleAlert.box.width")
