80b0b957eddc34ca3d99586828ef451fb0c5f848
Dan Rosenstark <dan@dr2050.com>
2024-07-06 16:54:16 -0400

    Lots of fixes over much time

--- SimpleAlertExample/SimpleAlertExample/ViewController.swift
+++ SimpleAlertExample/SimpleAlertExample/ViewController.swift
@@ -7,9 +7,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         view.backgroundColor = UIColor.orange
         showFirstAlert()
-        for i in 1 ... 5 {
-            showThirdAlert(i)
-        }
+//        for i in 1 ... 5 {
+//            showThirdAlert(i)
+//        }
     }
 
     @IBAction func showFirstAlert() {
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
 
         alert.addButtonWithTitle("Another Alert, Dark") {
             self.showSecondAlert(false)
+        }
+
+        alert.addButtonWithTitle("Do the picker") {
+            self.showPickerAlert()
         }
 
         alert.showInWindow(view.window!)
@@ -47,5 +51,115 @@ class ViewController: UIViewController {
         alert.addButtonWithTitle("OK", block: {})
         alert.theme = .light
         alert.showInWindow(view.window!)
+    }
+
+    func showPickerAlert() {
+        let alertText = "This is an alert with a picker.\nIsn't it nice to have such a handy tool?\nIt makes selection so much easier!"
+        let alert = SimpleAlert.makeAlert("Title!", message: alertText)
+        let pickerView = TestPickerView()
+        pickerView.backgroundColor = .white
+        pickerView.tintColor = .black
+        pickerView.layer.cornerRadius = pickerView.frame.size.height * 0.1
+        pickerView.layer.borderColor = UIColor.black.cgColor
+        pickerView.layer.borderWidth = 1.0
+
+        alert.setCustomView(pickerView)
+        let okButton = alert.addButtonWithTitle("OK", block: {})
+        alert.doNotAutomaticallyEnableTheseButtons = [okButton]
+        alert.addButtonWithTitle("Cancel", block: {})
+
+        pickerView.setOnDirtyStateChangedHandler { isDirty in
+            okButton.isEnabled = isDirty
+        }
+
+        alert.theme = .dark
+        alert.showInWindow(view.window!)
+    }
+}
+
+class PickerViewWithDirtyCleanState: UIPickerView, UIPickerViewDelegate {
+    private var onDirtyStateChanged: ((Bool) -> Void)?
+    private var initialValuesByComponent: [Int: Int]?
+
+    public var areAllComponentsDirty: Bool = false {
+        didSet {
+            onDirtyStateChanged?(areAllComponentsDirty)
+        }
+    }
+
+    override var dataSource: UIPickerViewDataSource? {
+        didSet {
+            setCleanState()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        delegate = self
+    }
+
+    func setOnDirtyStateChangedHandler(_ handler: ((Bool) -> Void)?) {
+        onDirtyStateChanged = handler
+    }
+
+    func setCleanState() {
+        var initialValue = [Int: Int]()
+        let numberOfComponents = self.numberOfComponents
+        for component in 0 ..< numberOfComponents {
+            let selectedRow = self.selectedRow(inComponent: component)
+            initialValue[component] = selectedRow
+        }
+
+        self.initialValuesByComponent = initialValue
+
+        areAllComponentsDirty = false
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        guard let initialValuesByComponent else { return }
+
+        // Check if all components are dirty
+        areAllComponentsDirty = initialValuesByComponent.allSatisfy { $0.value != pickerView.selectedRow(inComponent: $0.key) }
+    }
+}
+
+class TestPickerView: PickerViewWithDirtyCleanState, UIPickerViewDataSource {
+    let fruits = ["None", "Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Kiwi", "Lemon", "Mango", "Nectarine", "Orange", "Papaya", "Quince"]
+    let preparations = ["None", "Sliced", "Diced", "Whole", "Juiced", "Pureed"]
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        delegate = self
+        dataSource = self
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return fruits.count
+        } else {
+            return preparations.count
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return fruits[row]
+        } else {
+            return preparations[row]
+        }
     }
 }
--- SimpleAlertLib/UIView+Util.swift
+++ SimpleAlertLib/UIView+Util.swift
@@ -2,12 +2,21 @@
 
 import UIKit
 
-extension UIView {
-    func constrainCenterTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
-        guard let view else { return }
+// MARK: - Autolayout
 
-        NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerX, multiplier: multiplierX, constant: 0).activateAndName("simpleAlert.box.centerX")
-        NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerY, multiplier: multiplierY, constant: 0).activateAndName("simpleAlert.box.centerY")
+extension UIView {
+    func constrainCenterTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) -> [NSLayoutConstraint] {
+        guard let view else { return [NSLayoutConstraint]() }
+
+        return [NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerX, multiplier: multiplierX, constant: 0).activateAndName("simpleAlert.box.centerX"),
+        NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerY, multiplier: multiplierY, constant: 0).activateAndName("simpleAlert.box.centerY")]
+    }
+    
+    func constrainCenterAndTopTo(view: UIView?, marginY: CGFloat = 1.0) -> [NSLayoutConstraint] {
+        guard let view else { return [NSLayoutConstraint]() }
+
+        return [NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerX, multiplier: 1, constant: 0).activateAndName("simpleAlert.box.centerX"),
+                NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1.0, constant: marginY).activateAndName("simpleAlert.box.topY")]
     }
 
     func constrainSizeTo(view: UIView?, multiplierY: CGFloat = 1.0, multiplierX: CGFloat = 1.0) {
@@ -15,15 +24,5 @@ extension UIView {
 
         NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .width, multiplier: multiplierX, constant: 0).activateAndName("simpleAlert.box.width")
         NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .height, multiplier: multiplierY, constant: 0).activateAndName("simpleAlert.box.height")
-    }
-}
-
-extension NSLayoutConstraint {
-    func activateAndName(_ name: String, priority: UILayoutPriority? = nil) {
-        if let priority {
-            self.priority = priority
-        }
-        identifier = name
-        isActive = true
     }
 }
